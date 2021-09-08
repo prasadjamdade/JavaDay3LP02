5a372429a3a40bce3b633cfacec74a803267ef22
prasadjamdade <prasadjamdade@hotmail.com>
2021-09-08 19:00:11 +0530

    Added DisplayWelcome.java file to master

--- src/com/day3b/DisplayWelcome.java
+++ src/com/day3b/DisplayWelcome.java
@@ -2,6 +2,6 @@ package com.day3b;
 
 public class DisplayWelcome {
     public static void main(String[] args) {
-
+        System.out.println("Welcome to Line Comparison Computation Program");
     }
 }
